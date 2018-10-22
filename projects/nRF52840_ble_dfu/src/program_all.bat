9e3ef8c9c3a4d678697b8fb46695d3bd06a9dbc5
endlesscadence <22767373+endlesscadence@users.noreply.github.com>
2018-10-22 13:06:04 +0200

    DFU WIP

---
+++ projects/nRF52840_ble_dfu/src/program_all.bat
@@ -0,0 +1,9 @@
+SET OPENOCD=..\..\..\OpenOCD-master-20181020
+
+SET BOOTLOADER=../bootloader/_build/nrf52840_xxaa_s140.hex
+SET SOFTDEVICE=../softdevice/s140_nrf52_6.1.0_softdevice.hex
+SET PROGRAM=_build/nrf52840_xxaa.hex
+
+::%OPENOCD%\bin\openocd -s %OPENOCD%/share/openocd/scripts/ -f ../board/nRF52840_openocd.cfg -c init -c "reset init" -c halt -c "nrf5 mass_erase" -c "program %BOOTLOADER% verify" -c "program %SOFTDEVICE% verify" -c "program %PROGRAM% verify reset" -c reset -c exit
+::%OPENOCD%\bin\openocd -s %OPENOCD%/share/openocd/scripts/ -f ../board/nRF52840_openocd.cfg -c init -c "reset init" -c halt -c "nrf5 mass_erase" -c "program %SOFTDEVICE% verify" -c "program %PROGRAM% verify reset" -c reset -c exit
+%OPENOCD%\bin\openocd -s %OPENOCD%/share/openocd/scripts/ -f ../board/nRF52840_openocd.cfg -c init -c "reset init" -c halt -c "nrf5 mass_erase" -c "program %BOOTLOADER% verify" -c "program %SOFTDEVICE% verify reset" -c reset -c exit
